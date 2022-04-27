4d5fd6c26fcc818a38ba9cbb1d412ceaed06c265
heapwolf <paolo@async.ly>
2022-04-27 15:33:46 +0200

    improve example

--- tonic-ios/build.js
+++ tonic-ios/build.js
@@ -1,19 +1,14 @@
+#!/usr/bin/env node
+//
+// This example script will copy css and other files into the build directory.
+// It will also watch for changes and copy them when they are made. As a bonus
+// it adds static imports to the css compiler, just for fun.
+//
 const path = require('path')
 const fs = require('fs/promises')
 
 const Minifier = require('clean-css')
-const { build: js } = require('esbuild')
-
-//
-// The output target is passed by the build tool,
-// it's where we want to write all of our files.
-//
-const target = path.resolve(process.argv[2]);
-
-if (!target) {
-  console.log('Did not receive the build target path as an argument!')
-  process.exit(1)
-}
+const esbuild = require('esbuild')
 
 const minifier = new Minifier({ advanced: true })
 
@@ -41,21 +36,7 @@ const cp = async (a, b) => fs.cp(
   { recursive: true, force: true }
 )
 
-async function main () {
-  //
-  // Neither wkwebview or webview2 support local import. NBD,
-  // esbuild is fast, keeps the product small and reduces io
-  // in production.
-  //
-  await js({
-    entryPoints: ['src/index.js'],
-    bundle: true,
-    keepNames: true,
-    format: 'cjs',
-    // minify: true,
-    outfile: path.join(target, 'bundle.js')
-  })
-
+async function copy (target) {
   //
   // We want compile time @import so we can organize and
   // minify non-component styles.
@@ -71,6 +52,44 @@ async function main () {
   await cp('src/index.html', target)
   await cp(`src/icons/icon.png`, target)
   await cp('src/images', target)
+}
+
+async function main () {
+  const params = {
+    entryPoints: ['src/index.js'],
+    bundle: true,
+    keepNames: true,
+    format: 'cjs',
+    // minify: true,
+  }
+
+  //
+  // During development, this script will be started by npm and it
+  // will be passed the target directory for the build artifacts.
+  //
+  // During a build, this script will be called by the `op` command.
+  // In this case the target directory for the build artifacts will
+  // be provided as the argument at index 2.
+  //
+  let target = process.argv.find(s => s.includes('--watch='))
+
+  if (target) {
+    target = path.resolve(target.split('=')[1])
+
+    params.outfile = path.join(target, 'bundle.js')
+    esbuild.serve({ servedir: target }, params)
+  } else {
+    target = path.resolve(process.argv[2])
+    params.outfile = path.join(target, 'bundle.js')
+    await esbuild.build(params)
+  }
+
+  if (!target) {
+    console.log('Did not receive the build target path as an argument!')
+    process.exit(1)
+  }
+
+  copy(target)
 }
 
 main()
