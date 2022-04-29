bb3d0647f523389fd6a92715f401fb7fe6ecd306
Raynos <raynos2@gmail.com>
2022-04-29 12:54:20 -0500

    add test for ipc send/receive

--- tonic-desktop/test/render/index.js
+++ tonic-desktop/test/render/index.js
@@ -12,15 +12,24 @@ test('app-container exists', async (t) => {
   const common = await TestCommon.create()
   try {
     const container = common.container
-    t.ok(container)
-    t.ok(document.body.contains(container))
+    t.ok(container, 'the container exists')
+    t.ok(document.body.contains(container), 'AppContainer is in the body')
 
-    // const butIncr = document.querySelector('[data-event="inc"]')
-    // const butDeploy = document.querySelector('[data-event="deploy"]')
+    const sendInput = container.querySelector('#send input')
+    t.ok(sendInput, 'the send <input> exists')
 
-    // t.ok(butIncr)
-    // t.ok(butDeploy)
+    sendInput.value = 'hello'
+    sendInput.dispatchEvent(new Event('input', { bubbles: true }))
+
+    await sleep(250)
+
+    const receiveElem = container.querySelector('#response')
+    t.equal(receiveElem.value, 'hello', 'a response was received over IPC')
   } finally {
     await common.close()
   }
 })
+
+async function sleep (ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
