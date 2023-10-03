3ebf5675980ec7a5153db781378c3262e733da57
Bret Comnes <bcomnes@gmail.com>
2023-10-03 13:21:47 -0700

    Add another page example to demonstrate routing

--- frontend-tools-integrations/next-js/src/app/page.tsx
+++ frontend-tools-integrations/next-js/src/app/page.tsx
@@ -8,6 +8,9 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
         </p>
+        <p>
+          <a href="./another-page/">Click here to go to a different page</a>
+        </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
